7634562f4921c6f9495122901b566dd606c5fde7
Angad <angadgpb09876@gmail.com>
2023-11-11 19:44:23 +0530

    initial commit

--- index.js
+++ index.js
@@ -1,57 +1,50 @@
-// const grid = document.querySelector('.grid');
-// const startBtn = document.querySelector('.start-btn');
+const playBoard = document.querySelector('.grid');
+const scoreElement = document.querySelector('.score');
+const highScoreElement = document.querySelector('.high-score');
+const controls = document.querySelectorAll('.keys-container button');
+const startBtn = document.querySelector('.start-btn');
 
 
-// const width = 10;
-// const numCells = width * width;
-// let currentSnake = [2,1,0]
-// let snakeColor = Math.floor
-
-
-// grid.style.width = `${width * 10 * 2}px`;
-// grid.style.height = `${width * 10 * 2}px`;
-
-
-// for (let i = 0; i < width * width; i++) {
-//     const cell = document.createElement('div');
-//     cell.style.width = `${width * 2}px`;
-//     cell.style.height = `${width * 2}px`;
-//     grid.appendChild(cell);
-
-// }
-
-// const cells = document.querySelector('.grid div');
-
-
-// new game copy  
-const playBoard = document.querySelector('.play-board');
-
-
-
+let gameOver = false
 let foodX, foodY;
 let snakeX = 5, snakeY = 10;
 let snakeBody = [];
 let velocityX = 0, velocityY = 0;
+let setIntervalId;
+
+let score = 0;
+let highScore = localStorage.getItem('high-score') || 0;
+highScoreElement.innerText = `High Score: ${highScore} `;
+
+
 
 const changeFoodPosition = () => {
     foodX = Math.floor(Math.random() * 30) + 1;
     foodY = Math.floor(Math.random() * 30) + 1;
 }
+const HandleGameOver = () => {
+    clearInterval(setIntervalId)
+    // alert("Game Over! Press OK to replay...");
+    startBtn.addEventListener("click",() =>{
+        location.reload();
+    })
+    
+}
 
 const changeDirection = (e) => {
-    if (e.key === 'ArrowUp') {
+    if (e.key === 'ArrowUp' && velocityY != 1) {
         velocityX = 0;
         velocityY = -1;
     }
-    else if (e.key === 'ArrowDown') {
+    else if (e.key === 'ArrowDown' && velocityY != -1) {
         velocityX = 0;
         velocityY = 1;
     }
-    else if (e.key === 'ArrowLeft') {
+    else if (e.key === 'ArrowLeft' && velocityX != 1) {
         velocityX = -1;
         velocityY = 0;
     }
-    else if (e.key === 'ArrowRight') {
+    else if (e.key === 'ArrowRight' && velocityX != -1) {
         velocityX = 1;
         velocityY = 0;
     }
@@ -60,25 +53,53 @@ const changeDirection = (e) => {
 
 }
 
+controls.forEach(key =>{
+    key.addEventListener("click",() => changeDirection({key: key.dataset.key}));
+})
+
 
 const initGame = () => {
+    if(gameOver) return HandleGameOver();
     let htmlMark = `<div class = "food" style = "grid-area: ${foodY} / ${foodX}"></div>`;
-    if(snakeX ===foodX && snakeY === foodY){
+    if (snakeX === foodX && snakeY === foodY) {
         changeFoodPosition();
-        snakeBody.push([foodX,foodY])
+        snakeBody.push([foodX, foodY])
+        score++;
+        highScore = score >= highScore ? score: highScore;
+        localStorage.setItem('high-score', highScore);
+        scoreElement.innerText = `Score: ${score} `;
+        highScoreElement.innerText = `High Score: ${highScore} `;
+        // console.log(snakeBody);
     }
+    for(let i = snakeBody.length-1; i> 0; i--){
+        snakeBody[i] = snakeBody[i-1]
+    } 
+    snakeBody[0] = [snakeX ,snakeY];
+ 
+
+
     snakeX += velocityX;
     snakeY += velocityY;
-    htmlMark += `<div class = "head" style = "grid-area: ${snakeY} / ${snakeX}"></div>`;
+
+    if(snakeX <= 0 || snakeX > 30 || snakeY <= 0 || snakeY > 30){
+        gameOver = true;
+    }
+
+    for (let i = 0; i < snakeBody.length; i++) {
+        htmlMark += `<div class = "head" style = "grid-area: ${snakeBody[i][1]} / ${snakeBody[i][0]}"></div>`;
+        if(i !==0 && snakeBody[0][1] === snakeBody[i][1] && snakeBody[0][0] === snakeBody[i][0]){
+            gameOver = true;
+        }
+
+    }
     playBoard.innerHTML = htmlMark;
 }
+
+
 changeFoodPosition();
-setInterval(initGame, 125);
- // initGame();
-
-
+setIntervalId = setInterval(initGame, 125);
+// initGame();
 document.addEventListener('keydown', changeDirection)
 
 
-// new game copy  
-
+ 
